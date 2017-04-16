397d91c155f78435ce14377b9564ff883a484ff6
Sharan Naribole <sn23@rice.edu>
2017-04-16 12:26:08 -0500

    Create simulate.py

---
+++ simulate.py
@@ -0,0 +1,9 @@
+    def fellaini_performance(match_result):
+    """
+    Function to simulate a typical United fan's review
+    of Fellaini's performance
+    """
+        if result == 'win':    
+            print("FELLAINI IS OUTSTANDING")    
+        else:
+            print("FELLAINI, YOU USELESS PINE TREE")
